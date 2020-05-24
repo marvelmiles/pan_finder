967fa14490d2a8fafba9dcb07fae9bb8bedd4311
Akinrinmola marvellous <marvellousabidemi2@gmail.com>
2020-05-24 15:17:43 +0100

    Updates

---
+++ precache-manifest.31a60f1b217f1e7af7f767c355029f21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2de8ce7df4552874f223735b7ecce82",
+    "url": "/pan_finder/index.html"
+  },
+  {
+    "revision": "77f44477398487696f6e",
+    "url": "/pan_finder/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1fe86af07dcabc378b75",
+    "url": "/pan_finder/static/css/main.70fd73c6.chunk.css"
+  },
+  {
+    "revision": "77f44477398487696f6e",
+    "url": "/pan_finder/static/js/2.f0ce4fa6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pan_finder/static/js/2.f0ce4fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fe86af07dcabc378b75",
+    "url": "/pan_finder/static/js/main.8fe48067.chunk.js"
+  },
+  {
+    "revision": "daf0a828fed87537aaf5",
+    "url": "/pan_finder/static/js/runtime-main.4028033b.js"
+  }
+]);
